23401e5c8014c8f78a5cd3351dc998492f920300
Nathan Schirle <naschirl@microsoft.com>
2021-06-16 14:49:29 -0700

    prettier fixes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
-const wiki = require('./Routes/Image/image');
+const wiki = require("./Routes/Image/image");
 // ...
-app.use('/wiki', wiki);
-app.get('/', (req, res) => {
-    res.send('Hello Test World!')
-  })
-  
+app.use("/wiki", wiki);
+app.get("/", (req, res) => {
+  res.send("Hello Test World!");
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+  console.log(`Example app listening at http://localhost:${port}`);
+});
